a1064fd7f5ac2da6edf7b01c8bba57be0b7f2281
Filipi <filipi.oliveira.garcia@gmail.com>
2025-12-13 15:08:53 +0100

    Subiendo mis respuestas al Roadmap

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/pow_fip.py
@@ -0,0 +1,30 @@
+#https://python.org
+
+# Comentario en una linea
+"""
+Comentario 
+en varias
+lineas
+utilizando 
+comillas dobles
+"""
+
+'''
+Comentario 
+en varias
+lineas
+utilizando
+comilla simple 
+'''
+
+mi_variable = "Hola, esta es mi variable"
+#Aunque se pueda modificar, al poner todo en mayuscula, indico que es una constante
+MI_CONSTANTE = "Hola, esta es mi constante" 
+
+tipo_str = "Hola, soy una cadena de texto"
+tipo_int = 1 # Un entero
+tipo_float = 1.89 # Un número flotante
+#Tipo booleano ⏬
+tipo_bool = True
+tipo_bool = False 
+print("Hola, Python👋")
